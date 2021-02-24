f7c0d4919e93d258d4b99f3c53d9bbc725b28a46
Michael Tarantino <taramic.42@gmail.com>
2021-02-24 11:03:44 -0500

    Added a Hub scene for later testing purposes with DontDestroyOnLoad. Added prefab for SceneChangeTriggers. Scene transitions likely will be added at a later date. Pretty sure next think to add will be a game manager.

---
+++ Assets/Scripts/Game Management/SceneChangeTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeTrigger : MonoBehaviour
+{
+    [SerializeField] int targetSceneIndex;
+
+   public void ChangeScene()
+    {
+        SceneManager.LoadScene(targetSceneIndex);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        ChangeScene();
+    }
+
+}
